80ee59d22732054ae31163d2c59be6327ddcb9c1
Daniel Ogonowski <daniel.ogonowski@gmail.com>
2025-07-20 17:30:34 +0200

    add wages table

--- src/main.js
+++ src/main.js
@@ -21,7 +21,115 @@ const state = {
     },
   },
   bestWorkers: {},
+  rankElementsMap: new Map(),
+  // skill.tier.[minWage, maxWage]
+  wageTableWages: {
+    fa: {
+      s: { 1: [], 2: [], 3: [], 4: [], 5: [] },
+      a: { 1: [], 2: [], 3: [], 4: [], 5: [] },
+      b: { 1: [], 2: [], 3: [], 4: [], 5: [] },
+      c: { 1: [], 2: [], 3: [], 4: [], 5: [] },
+      d: { 1: [], 2: [], 3: [], 4: [], 5: [] },
+      e: { 1: [], 2: [], 3: [], 4: [], 5: [] },
+      f: { 1: [], 2: [], 3: [], 4: [], 5: [] },
+    },
+    sm: {
+      s: { 1: [], 2: [], 3: [], 4: [], 5: [] },
+      a: { 1: [], 2: [], 3: [], 4: [], 5: [] },
+      b: { 1: [], 2: [], 3: [], 4: [], 5: [] },
+      c: { 1: [], 2: [], 3: [], 4: [], 5: [] },
+      d: { 1: [], 2: [], 3: [], 4: [], 5: [] },
+      e: { 1: [], 2: [], 3: [], 4: [], 5: [] },
+      f: { 1: [], 2: [], 3: [], 4: [], 5: [] },
+    },
+    mi: {
+      s: { 1: [], 2: [], 3: [], 4: [], 5: [] },
+      a: { 1: [], 2: [], 3: [], 4: [], 5: [] },
+      b: { 1: [], 2: [], 3: [], 4: [], 5: [] },
+      c: { 1: [], 2: [], 3: [], 4: [], 5: [] },
+      d: { 1: [], 2: [], 3: [], 4: [], 5: [] },
+      e: { 1: [], 2: [], 3: [], 4: [], 5: [] },
+      f: { 1: [], 2: [], 3: [], 4: [], 5: [] },
+    },
+    sh: {
+      s: { 1: [], 2: [], 3: [], 4: [], 5: [] },
+      a: { 1: [], 2: [], 3: [], 4: [], 5: [] },
+      b: { 1: [], 2: [], 3: [], 4: [], 5: [] },
+      c: { 1: [], 2: [], 3: [], 4: [], 5: [] },
+      d: { 1: [], 2: [], 3: [], 4: [], 5: [] },
+      e: { 1: [], 2: [], 3: [], 4: [], 5: [] },
+      f: { 1: [], 2: [], 3: [], 4: [], 5: [] },
+    },
+    di: {
+      s: { 1: [], 2: [], 3: [], 4: [], 5: [] },
+      a: { 1: [], 2: [], 3: [], 4: [], 5: [] },
+      b: { 1: [], 2: [], 3: [], 4: [], 5: [] },
+      c: { 1: [], 2: [], 3: [], 4: [], 5: [] },
+      d: { 1: [], 2: [], 3: [], 4: [], 5: [] },
+      e: { 1: [], 2: [], 3: [], 4: [], 5: [] },
+      f: { 1: [], 2: [], 3: [], 4: [], 5: [] },
+    },
+    ma: {
+      s: { 1: [], 2: [], 3: [], 4: [], 5: [] },
+      a: { 1: [], 2: [], 3: [], 4: [], 5: [] },
+      b: { 1: [], 2: [], 3: [], 4: [], 5: [] },
+      c: { 1: [], 2: [], 3: [], 4: [], 5: [] },
+      d: { 1: [], 2: [], 3: [], 4: [], 5: [] },
+      e: { 1: [], 2: [], 3: [], 4: [], 5: [] },
+      f: { 1: [], 2: [], 3: [], 4: [], 5: [] },
+    },
+  },
+  wageTableActiveSkill: 'fa',
 };
+
+function calculateWorkersFromDB(data) {
+  const { workers, skills } = data;
+  state.workers = workers;
+  state.skills = skills;
+  for (let worker of state.workers) {
+    // fa,sn,mi,sh,di,ma
+    for (let [skillName, outputs] of Object.entries(state.skills)) {
+      const tier = worker[skillName];
+      // Worker Check State Setup
+      const bestScore = state.bestScores[skillName];
+      if (tier === 0) continue;
+      const currentWorkerScore = calculateScore(
+        skillName,
+        worker.w,
+        outputs[tier]
+      );
+      worker[`score-${skillName}`] = currentWorkerScore;
+      if (currentWorkerScore > bestScore) {
+        state.bestScores[skillName] = currentWorkerScore;
+        state.bestWorkers[skillName] = worker;
+      }
+    }
+  }
+}
+
+// WORKER CHECK ELEMENT
+function createRankElementsMap() {
+  const rankElementsArr = [...document.querySelectorAll('.rank')];
+  for (let rankEl of rankElementsArr) {
+    state.rankElementsMap.set(rankEl.dataset.skill, {
+      letterEl: rankEl.querySelector('.letter'),
+      percentageEl: rankEl.querySelector('.percentage'),
+    });
+  }
+}
+
+function calculateScore(skillName, wage, output) {
+  if (
+    skillName === 'fa' ||
+    skillName === 'sm' ||
+    skillName === 'mi' ||
+    skillName === 'sh'
+  ) {
+    return output / wage;
+  } else if (skillName === 'di' || skillName === 'ma') {
+    return output[0] / output[1] / wage;
+  }
+}
 
 function getRank(score, bestScore) {
   const ratio = score / bestScore;
@@ -44,51 +152,39 @@ function getRank(score, bestScore) {
   return { rank, percentageScore: Number((ratio * 100).toFixed()) };
 }
 
-function calculateScore(skill, wage, output) {
-  if (skill === 'fa' || skill === 'sm' || skill === 'mi' || skill === 'sh') {
-    return output / wage;
-  } else if (skill === 'di' || skill === 'ma') {
-    return output[0] / output[1] / wage;
-  }
-}
-
-function calculateWorkersFromDB(data) {
-  const { workers, skills } = data;
-  state.skills = skills;
-  for (let worker of workers) {
-    // fa,sn,mi,sh,di,ma
-    for (let [skill, outputs] of Object.entries(state.skills)) {
-      const tier = worker[skill];
-      const bestScore = state.bestScores[skill];
-      const currentWorkerScore = calculateScore(skill, worker.w, outputs[tier]);
-      if (currentWorkerScore > bestScore) {
-        state.bestScores[skill] = currentWorkerScore;
-        state.bestWorkers[skill] = worker;
-      }
-    }
-  }
+function updateRank(skillName) {
+  const enteredWorkerSkillObj = state.enteredWorker.skills[skillName];
+  const tier = enteredWorkerSkillObj.tier;
+  if (tier === null) return;
+  const score = enteredWorkerSkillObj.score;
+  const { rank, percentageScore } = getRank(score, state.bestScores[skillName]);
+  state.rankElementsMap.get(skillName).letterEl.textContent = rank;
+  state.rankElementsMap.get(
+    skillName
+  ).percentageEl.textContent = `${percentageScore}%`;
 }
 
 function updateRanks() {
-  const rankElements = document.querySelectorAll('.rank');
-  for (let rankEl of rankElements) {
-    updateRank(rankEl);
+  for (let skillName of Object.keys(state.skills)) {
+    updateRank(skillName);
   }
 }
 
-function updateRank(rankEl) {
-  const skillName = rankEl.dataset.skill;
+function resetRank(skillName) {
   const enteredWorkerSkillObj = state.enteredWorker.skills[skillName];
-  const score = enteredWorkerSkillObj.score;
-  const tier = enteredWorkerSkillObj.tier;
-  if (tier === null) return;
-  const { rank, percentageScore } = getRank(score, state.bestScores[skillName]);
-  rankEl.textContent = rank;
-  rankEl.nextElementSibling.textContent = `${percentageScore}%`;
+  enteredWorkerSkillObj.score = 0;
+  state.rankElementsMap.get(skillName).letterEl.textContent = '';
+  state.rankElementsMap.get(skillName).percentageEl.textContent = '';
 }
 
-function setEventListeners() {
-  const inputsContainer = document.querySelector('.worker');
+function resetRanks() {
+  for (let skillName of Object.keys(state.skills)) {
+    resetRank(skillName);
+  }
+}
+
+function setWorkerCheckEventListeners() {
+  const inputsContainer = document.querySelector('.worker-check');
   inputsContainer.addEventListener('change', event => {
     const target = event.target;
     switch (target.type) {
@@ -97,21 +193,22 @@ function setEventListeners() {
         if (enteredValue <= 0) {
           target.value = '';
           state.enteredWorker.w = null;
-          return;
+          resetRanks();
+        } else {
+          state.enteredWorker.w = enteredValue;
+          // update scores for all skills
+          for (let [skillName, skillObj] of Object.entries(
+            state.enteredWorker.skills
+          )) {
+            if (skillObj.tier === null) continue;
+            skillObj.score = calculateScore(
+              skillName,
+              state.enteredWorker.w,
+              state.skills[skillName][skillObj.tier]
+            );
+          }
+          updateRanks();
         }
-        state.enteredWorker.w = enteredValue;
-        // update scores for all skills
-        for (let [skillName, skillObj] of Object.entries(
-          state.enteredWorker.skills
-        )) {
-          if (skillObj.tier === null) continue;
-          skillObj.score = calculateScore(
-            skillName,
-            state.enteredWorker.w,
-            state.skills[skillName][skillObj.tier]
-          );
-        }
-        updateRanks();
         break;
       case 'radio':
         const skillName = `${target.name[0]}${target.name[1]}`;
@@ -124,15 +221,84 @@ function setEventListeners() {
           state.enteredWorker.w,
           state.skills[skillName][tier]
         );
-        updateRank(document.querySelector(`.rank[data-skill=${skillName}]`));
+        updateRank(skillName);
         break;
     }
   });
 }
 
+function setupWorkerCheck() {
+  createRankElementsMap();
+  setWorkerCheckEventListeners();
+}
+
+// WORKERS WAGE TABLE
+function setupWageTable() {
+  // Wage Table State Setup
+  for (let worker of state.workers) {
+    const currentWorkerWage = worker.w;
+    for (let skillName of Object.keys(state.skills)) {
+      const currentSkilllScore = worker[`score-${skillName}`];
+      const currentSkillRank = getRank(
+        currentSkilllScore,
+        state.bestScores[skillName]
+      ).rank.toLowerCase();
+      const currentSkillTier = worker[skillName];
+      if (currentSkillTier === 0) continue;
+      const minMaxWage =
+        state.wageTableWages[skillName][currentSkillRank][currentSkillTier];
+      if (!minMaxWage.length) {
+        minMaxWage[0] = currentWorkerWage;
+        minMaxWage[1] = currentWorkerWage;
+      } else if (currentWorkerWage < minMaxWage[0]) {
+        minMaxWage[0] = currentWorkerWage;
+      } else if (currentWorkerWage > minMaxWage[1]) {
+        minMaxWage[1] = currentWorkerWage;
+      }
+    }
+  }
+}
+
+function resetWageTableView() {
+  const wageRangeEls = [...document.querySelectorAll('.wage-range')];
+  for (let wageRangeEl of wageRangeEls) {
+    wageRangeEl.textContent = '';
+  }
+}
+
+function updateWageTableView() {
+  resetWageTableView();
+  // update wages html
+  for (let [rank, tiersObject] of Object.entries(
+    state.wageTableWages[state.wageTableActiveSkill]
+  )) {
+    for (let [tier, minMaxArr] of Object.entries(tiersObject)) {
+      if (minMaxArr.length) {
+        document.querySelector(
+          `.${rank}t${tier}`
+        ).textContent = `${minMaxArr[0]}-${minMaxArr[1]}`;
+      }
+    }
+  }
+}
+function setWageTableEventListeners() {
+  document
+    .querySelector('.wage-table-skills')
+    .addEventListener('click', event => {
+      const target = event.target;
+      if (target.className === 'wage-table-skill') {
+        state.wageTableActiveSkill = target.dataset.skill;
+        updateWageTableView();
+      }
+    });
+}
+
 function main() {
   calculateWorkersFromDB(data);
-  setEventListeners();
+  setupWorkerCheck();
+  setupWageTable();
+  updateWageTableView();
+  setWageTableEventListeners();
 }
 
 window.onload = main;
